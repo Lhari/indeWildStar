c9cfb5f7e8bb272244d32b589d52e042aa7d6ed2
Lhari <kashan7@live.dk>
2014-07-06 17:31:38 +0200

    Joomla template update 	new file:   templates/WildStar/index.php 	new file:   templates/WildStar/sass/assets/_shame.scss 	new file:   templates/WildStar/sass/fontello.scss 	new file:   templates/WildStar/sass/general/_content.scss 	new file:   templates/WildStar/sass/general/_header.scss 	new file:   templates/WildStar/sass/general/_menu.scss 	new file:   templates/WildStar/sass/general/_nav.scss 	new file:   templates/WildStar/sass/general/_sidenav.scss 	new file:   templates/WildStar/sass/general/global.scss 	new file:   templates/WildStar/sass/main.scss 	new file:   templates/WildStar/sass/modules/_members.scss 	new file:   templates/WildStar/sass/modules/_recruitment.scss 	new file:   templates/WildStar/sass/modules/_slider.scss 	new file:   templates/WildStar/sass/responsive/_grid.scss 	new file:   templates/WildStar/sass/settings/_vars.scss 	new file:   templates/WildStar/sass/settings/vars.scss 	new file:   templates/WildStar/templateDetails.xml 	templates/wildstar/html/

---
+++ templates/WildStar/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <jdoc:include type="head" />
+        <link rel="stylesheet" href="templates/wildstartemplate/stylesheets/main.css" type="text/css" />
+        <script src="templates/wildstartemplate/all.top.min.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+        <div class="size-12 fixed-top" style="z-index: 99;">
+            <header>
+                <div class="grid size-12">
+                    <jdoc:include type="modules" name="position-1" style="xhtml" />
+                </div>
+            </header>
+            <nav style="z-index 999;">
+                <jdoc:include type="modules" name="position-2" style="xhtml" />
+            </nav>
+        </div>
+        <div class="grid size-12 slider">
+            <jdoc:include type="modules" name="position-3" style="xhtml" />
+        </div>
+        <div class="content-wrapper">
+           <div id="content" class="grid size-12">
+               <aside class="grid size-2 left">
+                    <jdoc:include type="modules" name="position-4" style="xhtml" />
+               </aside>
+                <section class="grid size-10 article">
+                    <jdoc:include type="component" />
+                </section>
+           </div> 
+        </div>
+        <footer class="grid size-12">
+            <jdoc:include type="modules" name="position-14" style="xhtml" />
+        </footer>
+    </body>
+</html>
